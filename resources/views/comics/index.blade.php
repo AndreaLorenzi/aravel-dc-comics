b95d409b85a4daca7c02b9cbd1bcead14c265f98
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-29 18:18:21 +0200

    add paginator and image

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,22 +1,33 @@
 @extends('layouts.base')
 
 @section('contents')
-@foreach ($comics as $comic)
-<div class="card" style="width: 18rem;">
-    <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{ $comic->series }}</h5>
-      {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+
+<div class="container">
+    {{ $comics->links() }}
+    <div class="row row-cols-3">
+        @foreach ($comics as $comic)
+        <div class="col">
+            <div class="card">
+            <img style="width: 100%" src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">{{ $comic->series }}</h5>
+            {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+            </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ $comic->price}}</li>
+            <li class="list-group-item">{{ $comic->title}}</li>
+            <li class="list-group-item">{{ $comic->type}}</li>
+            </ul>
+            <div class="card-body">
+            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link">show</a>
+            <a href="">create</a>
+            </div>
+        </div>
+        </div>
+        @endforeach
+        
     </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">{{ $comic->price}}</li>
-      <li class="list-group-item">{{ $comic->title}}</li>
-      <li class="list-group-item">{{ $comic->type}}</li>
-    </ul>
-    <div class="card-body">
-      <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link">show</a>
-      <a href="">create</a>
-    </div>
-  </div>
-@endforeach
+    {{ $comics->links() }}
+</div>
+
 @endsection
