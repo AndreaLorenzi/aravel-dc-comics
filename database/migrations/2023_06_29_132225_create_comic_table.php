311b6d167e39784754e93f7eb91273599e83f17b
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-29 15:45:38 +0200

    add arrey, and seeder

--- database/migrations/2023_06_29_132225_create_comic_table.php
+++ database/migrations/2023_06_29_132225_create_comic_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('comic', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 300);
+            $table->smallinteger('price');
+            $table->string('series', 100);
+            $table->string('sale_date', 20);
+            $table->string('type', 20);
+            $table->string('artists', 100);
+            $table->string('writers', 100);
             $table->timestamps();
         });
     }
