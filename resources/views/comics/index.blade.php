f6b748ed83dd6c755020b6045e18d6d5e8d29eef
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-29 17:54:25 +0200

    add show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,17 +5,17 @@
 <div class="card" style="width: 18rem;">
     <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">{{ $comic->title }}</h5>
+      <h5 class="card-title">{{ $comic->series }}</h5>
       {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item"></li>
-      <li class="list-group-item">A second item</li>
-      <li class="list-group-item">A third item</li>
+      <li class="list-group-item">{{ $comic->price}}</li>
+      <li class="list-group-item">{{ $comic->title}}</li>
+      <li class="list-group-item">{{ $comic->type}}</li>
     </ul>
     <div class="card-body">
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link">show</a>
+      <a href="">create</a>
     </div>
   </div>
 @endforeach
