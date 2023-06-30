bcbd92ad06863dec1a76ea8dcaea20191ebf6ffc
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-30 15:45:20 +0200

    add form and stile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,20 @@
 
 @section('contents')
         {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
-        <h1 class="text-center">
-            DC COMICS
-        </h1>
+        <h1 class="text-center text-light">Laravel Comics</h1>
+    <div class="d-flex flex-column">
+        
+        <div class="bat-btn">
+            <button class="bat_button">
+                <span><a href="{{ route('comics.index') }}">ARCHIVE COMICS
+                </a></span>
+            </button>
+        </div>
+        <div class="ins_btn">
+            <button class="ins_button">
+                <a href="{{ route('comics.create') }}">INSERT NEW COMIC</a>
+            </button>
+        </div>
+    </div>
 @endsection
  
