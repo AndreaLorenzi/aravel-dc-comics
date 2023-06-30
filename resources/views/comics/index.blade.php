6c38d2e3c395b3f03dee6948ccf1bc3825c484c0
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-30 16:42:13 +0200

    new card style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,28 +2,28 @@
 
 @section('contents')
 
-<div class="container">
-    {{ $comics->links() }}
+<div class="container my-5">
     <div class="row row-cols-3">
         @foreach ($comics as $comic)
         <div class="col">
-            <div class="card">
-            <img style="width: 100%" src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">{{ $comic->series }}</h5>
-            {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-            </div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">{{ $comic->price}}</li>
-            <li class="list-group-item">{{ $comic->title}}</li>
-            <li class="list-group-item">{{ $comic->type}}</li>
-            </ul>
-            <div class="card-body">
-            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link">show</a>
-            <a href="">create</a>
-            </div>
+            <div class="myCard">
+                <div class="innerCard">
+                    <div class="frontSide">
+                        <h5 class="card-title">{{ $comic->series }}</h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{ $comic->price}}</li>
+                            <li class="list-group-item">{{ $comic->title}}</li>
+                            <li class="list-group-item">{{ $comic->type}}</li>
+                            </ul>
+                    </div>
+                    <div class="backSide">
+                        <img style="width: 100%;height:100%;border-radius:10px" src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                    </div>
+                </div>
+            </div> 
+            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link text-center">show</a>
         </div>
-        </div>
+    
         @endforeach
         
     </div>
@@ -31,3 +31,18 @@
 </div>
 
 @endsection
+   {{-- <div class="card">
+            <img style="width: 100%" src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">{{ $comic->series }}</h5>
+            {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+            {{-- </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ $comic->price}}</li>
+            <li class="list-group-item">{{ $comic->title}}</li>
+            <li class="list-group-item">{{ $comic->type}}</li>
+            </ul>
+            <div class="card-body">
+            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="card-link text-center">show</a>
+            
+            </div>  --}}
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h1 class="text-center">
-        {{$comic->series}}
-    </h1>
-    <p>{{$comic->description}}</p>
+<div class="row">
+    <div class="col-4 offset-4">
+        <div class="card h-100" style="">
+            <img src={{ $comic->thumb }} class="image-fluid" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $comic->title }}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{ $comic->price }}</li>
+                <li class="list-group-item">{{ $comic->series }}</li>
+                <li class="list-group-item">{{ $comic->sale_date }}</li>
+                <li class="list-group-item">{{ $comic->type }}</li>
+            </ul>
+            <div class="card-body">
+                <p>{{ $comic->description }}</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
